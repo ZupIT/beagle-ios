8039d8d52f880f1aaead0fe0e434292d87a62290
Daniel Tes Carrasque <daniel@zup.com.br>
2022-04-05 16:23:59 -0300

    fix: Accessibility trait config in applyAccessibility
    Signed-off-by: Daniel Tes Carrasque <daniel@zup.com.br>

--- Sources/Beagle/Sources/Renderer/ViewConfigurator.swift
+++ Sources/Beagle/Sources/Renderer/ViewConfigurator.swift
@@ -93,8 +93,8 @@ class ViewConfigurator: ViewConfiguratorProtocol {
         }
         object?.isAccessibilityElement = accessibility.accessible
         
-        if let isHeader = accessibility.isHeader {
-            object?.accessibilityTraits = isHeader ? .header : .none
+        if let isHeader = accessibility.isHeader, isHeader {
+            object?.accessibilityTraits = .header
         }
     }
 }
